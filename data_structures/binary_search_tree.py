f1f8457d50aab9ed262a02147509d5373fe9052b
Andres Montero <andres.monterocassab@predictivelayer.com>
2022-02-26 14:59:04 +0100

    adding binary search structure

---
+++ data_structures/binary_search_tree.py
@@ -0,0 +1,109 @@
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self, data):
+        if data == self.data:
+            return  # node already exist
+
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+
+    def search(self, val):
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
+
+    def in_order_traversal(self):
+        elements = []
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        elements.append(self.data)
+
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return elements
+
+    def post_order_traversal(self):
+        elements = []
+        if self.left:
+            elements += self.left.post_order_traversal()
+
+        if self.right:
+            elements += self.right.post_order_traversal()
+
+        elements.append(self.data)
+
+        return elements
+
+    def pre_order_traversal(self):
+        elements = []
+
+        elements.append(self.data)
+
+        if self.left:
+            elements += self.left.pre_order_traversal()
+
+        if self.right:
+            elements += self.right.pre_order_traversal()
+
+        return elements
+
+    def find_min(self):
+        if self.left:
+            return self.left.find_min()
+        else:
+            return self.data
+
+    def find_max(self):
+        if self.right:
+            return self.right.find_max()
+        else:
+            return self.data
+
+    def calculate_sum(self):
+        return sum(self.in_order_traversal())
+
+
+def build_tree(elements):
+    print("Building tree with these elements:", elements)
+    root = BinarySearchTreeNode(elements[0])
+
+    for i in range(1, len(elements)):
+        root.add_child(elements[i])
+
+    return root
+
+
+numbers = [84, 102, 15, 1, 0, 10, 6, 5, 8, 98]
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+numbers_tree = build_tree(numbers)
+print(numbers_tree.in_order_traversal())
+print(numbers_tree.post_order_traversal())
+print(numbers_tree.pre_order_traversal())
+print(numbers_tree.find_min())
+print(numbers_tree.find_max())
+print(numbers_tree.calculate_sum())
