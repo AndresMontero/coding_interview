b3dae0def659c57b4cbc56fe03f640e2ce0e291a
Andres Montero <andres.monterocassab@predictivelayer.com>
2022-02-28 13:38:29 +0100

    finding all occurrences of a number inside binary search

--- algorithms/my_binary_search.py
+++ algorithms/my_binary_search.py
@@ -32,6 +32,39 @@ def binary_search(numbers_list, number_to_find):
     return -1
 
 
+@time_it
+def binary_search_all_occurrences(numbers_list, number_to_find):
+    left_index = 0
+    right_index = len(numbers_list) - 1
+    answer = []
+    while left_index <= right_index:
+        mid_index = (left_index + right_index) // 2
+        mid_number = numbers_list[mid_index]
+
+        if mid_number == number_to_find:
+            answer.append(mid_index)
+
+            tmp = mid_index + 1
+            while numbers_list[tmp] == mid_number:
+                answer.append(tmp)
+                tmp += 1
+
+            tmp = mid_index - 1
+            while numbers_list[tmp] == mid_number:
+                answer.append(tmp)
+                tmp -= 1
+
+            return sorted(answer)
+
+        if mid_number < number_to_find:
+
+            left_index = mid_index + 1
+        else:
+            right_index = mid_index - 1
+
+    return -1
+
+
 def binary_search_recursive(numbers_list, number_to_find, left_index, right_index):
     if right_index < left_index:
         return -1
@@ -101,3 +134,4 @@ if __name__ == '__main__':
     print(f"The index of the number is {index_binary_rec}")
 
     print(f"All the occurrences are  {find_all_occurrences(numbers_list, number_to_find)}")
+    print(f"All the occurrences are  {binary_search_all_occurrences(numbers_list, number_to_find)}")
