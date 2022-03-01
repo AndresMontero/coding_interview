ba5623c4106d4dab83dc234cbe100612eb905896
Andres Montero <andres.monterocassab@predictivelayer.com>
2022-03-01 11:26:23 +0100

    created bubble sort algorithm

---
+++ algorithms/my_buble_sort.py
@@ -0,0 +1,39 @@
+### Here you are comparing element and index i with all the rest, not pair comparison
+def modified_bubble_sort(elements):
+    size = len(elements)
+    count = 0
+    for i in range(size - 1):
+        for j in range(i, size):
+            count += 1
+            if elements[i] > elements[j]:
+                tmp = elements[j]
+                elements[j] = elements[i]
+                elements[i] = tmp
+
+    print(f"Total number of times modified_bubble_sort: {count}")
+
+    return elements
+
+
+def bubble_sort(elements):
+    size = len(elements)
+    count = 0
+    for i in range(size - 1):
+        for j in range(0, size - 1 - i):
+            count += 1
+            if elements[j] > elements[j + 1]:
+                tmp = elements[j + 1]
+                elements[j + 1] = elements[j]
+                elements[j] = tmp
+    print(f"Total number of times bubble sort: {count}")
+    return elements
+
+
+if __name__ == '__main__':
+    elements = [5, 9, 2, 1, 67, 34, 88, 34]
+    bubble_sort(elements)
+    print(elements)
+
+    elements = [5, 9, 2, 1, 67, 34, 88, 34]
+    modified_bubble_sort(elements)
+    print(elements)
