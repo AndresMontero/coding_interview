ef9f4dd8227c356c96ea5f010f9d1cf4df3d8bb2
Andres Ivan Montero Cassab <andres.montero.cassab@gmail.com>
2022-03-01 21:54:31 +0100

    quick sort working

--- algorithms/my_quick_sort.py
+++ algorithms/my_quick_sort.py
@@ -1,7 +1,4 @@
 def partition(elements, start, end):
-    # pivot = 0
-    # start = pivot + 1
-    # end = len(elements) - 1
     start_init = start
     pivot = start_init - 1
     pivot_value = elements[pivot]
@@ -18,7 +15,7 @@ def partition(elements, start, end):
                     elements[end] = tmp
                 else:
                     end -= 1
-            elif start == len(elements)-1:
+            elif start == end:
                 tmp = elements[start]
                 elements[start] = elements[pivot]
                 elements[pivot] = tmp
@@ -37,21 +34,24 @@ def partition(elements, start, end):
 
 
 def quick_sort(elements, start, end):
-    start_i, end_i = partition(elements, start, end)
-
-    quick_sort(elements, start, end_i - 1)
-    quick_sort(elements, start_i + 1, end)
-    print(end_i)
+    if start < end:
+        start_i, end_i = partition(elements, start, end)
+        quick_sort(elements, start, end_i - 1)
+        quick_sort(elements, start_i + 1, end)
 
     return elements
 
 
 if __name__ == '__main__':
-    elements = [11, 9, 29, 7, 2, 15, 28]
-    partition(elements, 1, len(elements) - 1)
-    print(elements)
-    partition(elements, 4+1, len(elements) - 1)
-    print(elements)
+    tests = [
+        [11, 9, 29, 7, 2, 15, 28],
+        [3, 7, 9, 11],
+        [25, 22, 21, 10],
+        [29, 15, 28],
+        [],
+        [6]
+    ]
 
-    quick_sort(elements, 1, len(elements) - 1)
-    print(elements)
+    for elements in tests:
+        quick_sort(elements, 1, len(elements) - 1)
+        print(f'sorted array: {elements}')
