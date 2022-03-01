bacd14542a0e4791a6dd5732d9fc08d6382c8773
Andres Ivan Montero Cassab <andres.montero.cassab@gmail.com>
2022-03-01 22:08:14 +0100

    added comments to code

--- algorithms/my_quick_sort.py
+++ algorithms/my_quick_sort.py
@@ -1,20 +1,28 @@
 def partition(elements, start, end):
     start_init = start
+    # pivot will be first element of the array, this could be optimized to avoid worst
+    # case scenario (choose middle value for example)
     pivot = start_init - 1
     pivot_value = elements[pivot]
     while True:
+
+        if start < 0 or end > len(elements) + 1:
+            break
         start_value = elements[start]
         end_value = elements[end]
         if start <= end:
+            # Compare values using pivot
             if start_value > pivot_value:
-
                 if end_value < pivot_value:
-
                     tmp = elements[start]
                     elements[start] = elements[end]
                     elements[end] = tmp
+                    start += 1
+                    end -= 1
                 else:
                     end -= 1
+
+            # If start is the same as end, this means pivot is the biggest number, swap pivot_value with start_value
             elif start == end:
                 tmp = elements[start]
                 elements[start] = elements[pivot]
@@ -28,15 +36,17 @@ def partition(elements, start, end):
             elements[end] = elements[pivot]
             elements[pivot] = tmp
             break
-        if start < 0 or end > len(elements) + 1:
-            break
+
     return start, end
 
 
 def quick_sort(elements, start, end):
     if start < end:
+        # First find the first partition
         start_i, end_i = partition(elements, start, end)
+        # Continue sorting left array
         quick_sort(elements, start, end_i - 1)
+        # Continue sorting right array
         quick_sort(elements, start_i + 1, end)
 
     return elements
