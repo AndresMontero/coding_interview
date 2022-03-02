c775b3bb9193327e4f59cf3e5ce007133e769291
Andres Ivan Montero Cassab <andres.montero.cassab@gmail.com>
2022-03-02 15:35:55 +0100

    fixes 2

--- algorithms/my_quick_sort_l.py
+++ algorithms/my_quick_sort_l.py
@@ -3,48 +3,24 @@
 def partition(elements, start, end):
     pivot_value = elements[end]
     p_i = start
-    to_return = 0
-    # start_value = elements[start]
-    # for p_i in range(start, end):
     if end > 0 and start < len(elements):
         while p_i < end:
-            if elements[p_i] <= pivot_value:
-                pass
-            else:
-                # while i < end +1:
+            if elements[p_i] > pivot_value:
                 for i in range(p_i + 1, end + 1):
                     if elements[i] <= pivot_value:
                         tmp = elements[i]
                         elements[i] = elements[p_i]
                         elements[p_i] = tmp
-                        if p_i == start:
-                            to_return = i + 1
-                        else:
-                            to_return = p_i
                         break
-                    # i += 1
-                    # if i == end and start_value > pivot_value:
-                    #     tmp = elements[i]
-                    #     elements[i] = elements[start]
-                    #     elements[start] = tmp
 
             p_i += 1
-
-    # if elements[p_i-1] > elements[end]:
-    #     tmp = elements[end]
-    #     elements[end] = elements[p_i-1]
-    #     elements[p_i-1] = tmp
-    # else:
-    #     tmp = elements[start]
-    #     elements[start] = elements[p_i - 1]
-    #     elements[p_i - 1] = tmp
-    return to_return
+    return p_i
 
 
 def quick_sort_l(elements, start, end):
     if len(elements) < 2:
         return
-    if start < end:
+    if start < end and end > 0 and start < len(elements):
         # First find the first partition
         p_i = partition(elements, start, end)
         # Continue sorting left array
@@ -73,6 +49,6 @@ if __name__ == '__main__':
     # elements = [25, 22, 21, 10]
     # quick_sort_l(elements, 0, len(elements) - 1)
     # print(elements)
-    # for elements in tests:
-    #     quick_sort_l(elements, 0, len(elements) - 1)
-    #     print(f'sorted array: {elements}')
+    for elements in tests:
+        quick_sort_l(elements, 0, len(elements) - 1)
+        print(f'sorted array: {elements}')
