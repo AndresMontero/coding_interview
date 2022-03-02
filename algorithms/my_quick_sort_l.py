96a4abb3c53771d8fd9d8cfc8f26edd7c7e7d299
Andres Ivan Montero Cassab <andres.montero.cassab@gmail.com>
2022-03-02 14:40:10 +0100

    almost there right sort not working

--- algorithms/my_quick_sort_l.py
+++ algorithms/my_quick_sort_l.py
@@ -3,18 +3,42 @@
 def partition(elements, start, end):
     pivot_value = elements[end]
     p_i = start
+    to_return = 0
+    # start_value = elements[start]
+    # for p_i in range(start, end):
+    if end > 0 and start < len(elements):
+        while p_i < end:
+            if elements[p_i] <= pivot_value:
+                pass
+            else:
+                # while i < end +1:
+                for i in range(p_i + 1, end + 1):
+                    if elements[i] <= pivot_value:
+                        tmp = elements[i]
+                        elements[i] = elements[p_i]
+                        elements[p_i] = tmp
+                        if p_i == start:
+                            to_return = i + 1
+                        else:
+                            to_return = p_i
+                        break
+                    # i += 1
+                    # if i == end and start_value > pivot_value:
+                    #     tmp = elements[i]
+                    #     elements[i] = elements[start]
+                    #     elements[start] = tmp
 
-    for i in range(start, end):
-        if elements[i] <= pivot_value:
-            tmp = elements[i]
-            elements[i] = elements[p_i]
-            elements[p_i] = tmp
             p_i += 1
 
-    tmp = elements[end]
-    elements[end] = elements[p_i]
-    elements[p_i] = tmp
-    return p_i
+    # if elements[p_i-1] > elements[end]:
+    #     tmp = elements[end]
+    #     elements[end] = elements[p_i-1]
+    #     elements[p_i-1] = tmp
+    # else:
+    #     tmp = elements[start]
+    #     elements[start] = elements[p_i - 1]
+    #     elements[p_i - 1] = tmp
+    return to_return
 
 
 def quick_sort_l(elements, start, end):
@@ -33,6 +57,7 @@ def quick_sort_l(elements, start, end):
 
 if __name__ == '__main__':
     tests = [
+        [4, 2, 7, 3, 1, 9, 6, 0, 8],
         [11, 9, 29, 7, 2, 15, 28],
         [3, 7, 9, 11],
         [25, 22, 21, 10],
@@ -40,13 +65,14 @@ if __name__ == '__main__':
         [],
         [6]
     ]
-    # elements = [11, 9, 29, 7, 2, 15, 28]
-    # quick_sort_l(elements, 0, len(elements) - 1)
-    # print(elements)
+    elements = [4, 2, 7, 3, 1, 9, 6, 0, 8]
+
+    quick_sort_l(elements, 0, len(elements) - 1)
+    print(elements)
 
     # elements = [25, 22, 21, 10]
     # quick_sort_l(elements, 0, len(elements) - 1)
     # print(elements)
-    for elements in tests:
-        quick_sort_l(elements, 0, len(elements) - 1)
-        print(f'sorted array: {elements}')
+    # for elements in tests:
+    #     quick_sort_l(elements, 0, len(elements) - 1)
+    #     print(f'sorted array: {elements}')
