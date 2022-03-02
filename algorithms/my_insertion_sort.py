8e57ea3157694dda26cac3191c4ed4c1a1459272
Andres Ivan Montero Cassab <andres.montero.cassab@gmail.com>
2022-03-02 17:23:24 +0100

    running median code

--- algorithms/my_insertion_sort.py
+++ algorithms/my_insertion_sort.py
@@ -1,15 +1,40 @@
 def insertion_sort(elements):
     for i in range(1, len(elements)):
         anchor = elements[i]
-        for j in range(i-1, -1, -1):
+        for j in range(i - 1, -1, -1):
             if anchor < elements[j]:
-                elements[j+1] = elements[j]
+                elements[j + 1] = elements[j]
                 elements[j] = anchor
             else:
                 break
 
-
     return elements
+
+
+def running_median(elements):
+    if len(elements) < 1:
+        print(None)
+    if len(elements) < 2:
+        print(elements)
+
+    for i in range(1, len(elements)):
+        anchor = elements[i]
+
+        if i % 2 == 0:
+            a = elements[i//2]
+            b = elements[i//2 -1]
+            print((a+b)/2)
+        else:
+            print(elements[i//2])
+
+
+        for j in range(i - 1, -1, -1):
+            if anchor < elements[j]:
+                elements[j + 1] = elements[j]
+                elements[j] = anchor
+            else:
+                break
+
 
 
 if __name__ == '__main__':
@@ -25,3 +50,6 @@ if __name__ == '__main__':
     for elements in tests:
         insertion_sort(elements)
         print(f'sorted array: {elements}')
+
+
+    running_median([2, 1, 5, 7, 2, 0, 5])
