0da84487a04eafd315595e2148ea7de5639309ea
Andres Ivan Montero Cassab <andres.montero.cassab@gmail.com>
2022-03-03 17:03:21 +0100

    solution to excercise of removing duplicates

--- algorithms/my_shell_sort.py
+++ algorithms/my_shell_sort.py
@@ -5,7 +5,7 @@ def shell_sort_i(arr):
 
         for i in range(gap, size):
             anchor = arr[i]
-            for j in range(i-gap, -1, -gap):
+            for j in range(i - gap, -1, -gap):
                 if anchor <= arr[j]:
                     arr[j + gap] = arr[j]
                     arr[j] = anchor
@@ -23,15 +23,49 @@ def shell_sort_i(arr):
     #             arr[j] = anchor
 
 
+def shell_sort_i_no_duplicates(arr):
+    size = len(arr)
+    gap = size // 2
+    items_to_delete = []
+    while gap > 0:
+
+        for i in range(gap, size):
+            anchor = arr[i]
+            for j in range(i - gap, -1, -gap):
+                if anchor < arr[j]:
+                    arr[j + gap] = arr[j]
+                    arr[j] = anchor
+                elif anchor == arr[j]:
+                    items_to_delete.append(anchor)
+                else:
+                    break
+
+        items_to_delete = list(set(items_to_delete))
+        if items_to_delete:
+            for x in items_to_delete:
+                arr.remove(x)
+        size = len(arr)
+        items_to_delete = []
+        gap //= 2
+
+    # when the gap is 1 we do a normal insertion sort
+    # for m in range(1, len(arr)):
+    #     anchor = arr[m]
+    #     for j in range(m-1, -1, -1):
+    #         if anchor <= arr[j]:
+    #             arr[j + 1] = arr[j]
+    #             arr[j] = anchor
+
+
 def shell_sort_t(arr):
     size = len(arr)
-    gap = size//2
+    gap = size // 2
     while gap > 0:
-        for i in range(gap,size):
+        for i in range(gap, size):
             anchor = arr[i]
             j = i
-            while j>=gap and arr[j-gap]>anchor:
-                arr[j] = arr[j-gap]
+            while j >= gap and arr[j - gap] > anchor:
+                arr[j] = arr[j - gap]
                 j -= gap
             arr[j] = anchor
         gap = gap // 2
@@ -93,3 +127,8 @@ if __name__ == '__main__':
     for elements in tests:
         shell_sort_i(elements)
         print(f'sorted array: {elements}')
+
+    excercise = [2, 1, 5, 7, 2, 10, 10, 0, 5, 1, 2, 9, 5, 8, 3, 8]
+
+    shell_sort_i_no_duplicates(excercise)
+    print(f'sorted array: {excercise}')
