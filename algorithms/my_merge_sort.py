07cee695ae127227c9bd6491232b088262848c55
Andres Ivan Montero Cassab <andres.montero.cassab@gmail.com>
2022-03-03 11:19:10 +0100

    merge_sort working

--- algorithms/my_merge_sort.py
+++ algorithms/my_merge_sort.py
@@ -1,36 +1,54 @@
 def merge_two_sorted_lists(a, b):
     final_list = []
+    i = 0  # this will be the left index
     j = 0  # this will be the right index
-    for i in range(len(a)):
-        if a[i] < b[j]:
+    while i < len(a) and j < len(b):
+        if a[i] <= b[j]:
             final_list.append(a[i])
+            i += 1
         else:
             final_list.append(b[j])
             j += 1
+    if i < len(a):
+        final_list.extend(a[i:])
+    if j < len(b):
+        final_list.extend(b[j:])
 
-    final_list.extend(b[j:])
     return final_list
 
 
-def merge_sort(elemments):
-    pass
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+
+    left = arr[:mid]
+    right = arr[mid:]
+
+    left = merge_sort(left)
+    right = merge_sort(right)
+
+    return merge_two_sorted_lists(left, right)
 
 
 if __name__ == '__main__':
-    a = [1, 2, 3, 4]
-    b = [5, 6, 7, 8]
+    a = [1, 2, 3]
+    b = [8, 9, 10, 11, 12, 13, 14]
 
-    print(merge_two_sorted_lists(a,b))
+    a = [7]
+    b = [2]
+    print(merge_two_sorted_lists(a, b))
 
-    # tests = [
-    #     [11, 9, 29, 7, 2, 15, 28],
-    #     [3, 7, 9, 11],
-    #     [25, 22, 21, 10],
-    #     [29, 15, 28],
-    #     [],
-    #     [6]
-    # ]
-    #
-    # for elements in tests:
-    #     merge_sort(elements)
-    #     print(f'sorted array: {elements}')
+    tests = [
+        [11, 9, 29, 7, 2, 15, 28],
+        [3, 7, 9, 11],
+        [25, 22, 21, 10],
+        [29, 15, 28],
+        [],
+        [6]
+    ]
+
+    for elements in tests:
+        sorted_list = merge_sort(elements)
+        print(f'sorted array: {sorted_list}')
