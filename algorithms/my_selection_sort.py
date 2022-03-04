5f1be484c4d83c69e091b2385c307af8d86560f7
Andres Ivan Montero Cassab <andres.montero.cassab@gmail.com>
2022-03-04 11:38:10 +0100

    solution to exercise

--- algorithms/my_selection_sort.py
+++ algorithms/my_selection_sort.py
@@ -27,6 +27,23 @@ def selection_sort(arr):
             arr[j], arr[min_index] = arr[min_index], arr[j]
 
 
+def selection_sort_dict(arr, keys=[]):
+    size = len(arr)
+    # for key in keys:
+    for key in keys[::-1]:  # need to do the sorting in reverse order of what we received
+    # for key in keys:
+        for j in range(size - 1):  # iterate over the entire array
+            # This approach is without using function
+            min_index = j
+            for i in range(j + 1, size):  # find min index from current position forward
+                curr = arr[i][key]
+                if curr < arr[min_index][key]:
+                    min_index = i
+
+            if j != min_index:  # Only swap if min index is not already at correct position
+                arr[j], arr[min_index] = arr[min_index], arr[j]
+
+
 if __name__ == '__main__':
     test = [21, 38, 29, 17, 4, 25, 11, 32, 9]
     print(find_min(test, 0))
@@ -48,3 +65,25 @@ if __name__ == '__main__':
     for elements in tests:
         selection_sort(elements)
         print(f'sorted array: {elements}')
+
+    excercise = [
+        {'First Name': 'Raj', 'Last Name': 'Nayyar'},
+        {'First Name': 'Suraj', 'Last Name': 'Sharma'},
+        {'First Name': 'Karan', 'Last Name': 'Kumar'},
+        {'First Name': 'Jade', 'Last Name': 'Canary'},
+        {'First Name': 'Raj', 'Last Name': 'Thakur'},
+        {'First Name': 'Raj', 'Last Name': 'Sharma'},
+        {'First Name': 'Kiran', 'Last Name': 'Kamla'},
+        {'First Name': 'Armaan', 'Last Name': 'Kumar'},
+        {'First Name': 'Jaya', 'Last Name': 'Sharma'},
+        {'First Name': 'Ingrid', 'Last Name': 'Galore'},
+        {'First Name': 'Jaya', 'Last Name': 'Seth'},
+        {'First Name': 'Armaan', 'Last Name': 'Dadra'},
+        {'First Name': 'Ingrid', 'Last Name': 'Maverick'},
+        {'First Name': 'Aahana', 'Last Name': 'Arora'}
+    ]
+
+    selection_sort_dict(excercise, keys=['First Name', 'Last Name'])
+    for item in excercise:
+        print(item)
+    # print(f'sorted array: {excercise}')
