13c2aff6a7140a93741b1b1d652d133c8c334fc1
Andres Ivan Montero Cassab <andres.montero.cassab@gmail.com>
2022-03-06 15:27:19 +0100

    validate subsequence

---
+++ problems/validate_subsequence.py
@@ -0,0 +1,22 @@
+def isValidSubsequence(array, sequence):
+    # Time O(n)
+    # Space O(1)
+    arr_idx = 0
+    seq_idx = 0
+
+    while arr_idx < len(array) and seq_idx < len(sequence):
+
+        if array[arr_idx] == sequence[seq_idx]:
+            arr_idx += 1
+            seq_idx += 1
+        else:
+            arr_idx += 1
+
+    return seq_idx == len(sequence)
+
+
+if __name__ == '__main__':
+    array = [5, 1, 22, 25, 6, -1, 8, 10]
+    target = [1, 6, -1, 10]
+
+    print(isValidSubsequence(array, target))
