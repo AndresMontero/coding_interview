07d233057fe565895db136fa72c766f33e97f20b
Andres Ivan Montero Cassab <andres.montero.cassab@gmail.com>
2022-03-06 15:31:36 +0100

    validate subsequence with for loop

--- problems/validate_subsequence.py
+++ problems/validate_subsequence.py
@@ -15,8 +15,22 @@ def isValidSubsequence(array, sequence):
     return seq_idx == len(sequence)
 
 
+def isValidSubsequence_2(array, sequence):
+    # Time O(n)
+    # Space O(1)
+    seq_idx = 0
+
+    for n in array:
+        if seq_idx == len(sequence):
+            break
+        if n == sequence[seq_idx]:
+            seq_idx += 1
+    return seq_idx == len(sequence)
+
+
 if __name__ == '__main__':
     array = [5, 1, 22, 25, 6, -1, 8, 10]
     target = [1, 6, -1, 10]
 
     print(isValidSubsequence(array, target))
+    print(isValidSubsequence_2(array, target))
