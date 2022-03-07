78a55d7717d76800cb4dbef4e1c39e2a4625b10c
Andres Ivan Montero Cassab <andres.montero.cassab@gmail.com>
2022-03-07 09:57:54 +0100

    iterative solution to space complexity

--- problems/closest_value_BST.py
+++ problems/closest_value_BST.py
@@ -15,6 +15,27 @@ def findClosestValueInBst(tree, target, diff=100000, closest_val=None):
     return closest_val
 
 
+def findClosestValueInBst_it(tree, target, diff=100000, closest_val=None):
+    # Write your code here.
+    # Time O(log(n)) on average, worst case O(n)
+    # Space O(1) on average
+    current_node = tree
+    while current_node is not None:
+        if current_node.value == target:
+            return current_node.value
+        if abs(target - current_node.value) < diff:
+            closest_val = current_node.value
+            diff = abs(target - current_node.value)
+        if target < current_node.value:
+            current_node = current_node.left
+        elif target > current_node.value:
+            current_node = current_node.right
+        else:
+            break
+
+    return closest_val
+
+
 # This is the class of the input tree. Do not edit.
 class BST:
     def __init__(self, value):
@@ -36,4 +57,7 @@ if __name__ == '__main__':
     root.right.left.right = BST(14)
     root.right.right = BST(22)
     closest_val = findClosestValueInBst(root, 12)
+    print(closest_val)
+
+    closest_val = findClosestValueInBst_it(root, 12)
     print(closest_val)
