2c07d1cbae25525a7ae3f8610bc7ea41dd0db77d
Andres Ivan Montero Cassab <andres.montero.cassab@gmail.com>
2022-03-08 21:47:50 +0100

    three num sum

---
+++ problems/three_num_sum.py
@@ -0,0 +1,31 @@
+def threeNumberSum(array, targetSum):
+    # Write your code here.
+    result = []
+
+    array.sort()
+
+    for i in range(len(array)):
+
+        num_1 = array[i]
+        left = i+1
+        right = len(array) - 1
+        while left < right and left < len(array) - 1 and right > i:
+            num_2 = array[left]
+            num_3 = array[right]
+            if num_1 + num_2 + num_3 == targetSum:
+                result.append([num_1, num_2, num_3])
+                right -= 1
+                left += 1
+
+            elif num_1 + num_2 + num_3 < targetSum:
+                left += 1
+            else:
+                right -= 1
+
+    return result
+
+
+if __name__ == '__main__':
+    nums = [12, 3, 1, 2, -6, 5, -8, 6]
+    target = 0
+    print(threeNumberSum(nums, target))
