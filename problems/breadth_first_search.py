0f2ccb5d75f4c98f66f7b954d3c0724c66749105
Andres Ivan Montero Cassab <andres.montero.cassab@gmail.com>
2022-03-11 17:54:09 +0100

    added depth first search

--- problems/breadth_first_search.py
+++ problems/breadth_first_search.py
@@ -26,6 +26,21 @@ class Node:
 
         return array
 
+    def depth_first_search(self, array):
+        # Write your code here.
+        # Time O(nodes + edges)
+        # Space O(nodes)
+        stack = []
+
+        stack.append(self)
+        while len(stack) >= 1:
+            current = stack.pop()
+            array.append(current.name)
+            for children in current.children:
+                stack.append(children)
+
+        return array
+
 
 if __name__ == '__main__':
     graph = Node("A")
@@ -35,4 +50,4 @@ if __name__ == '__main__':
     graph.children[0].children[1].addChild("I").addChild("J")
     graph.children[2].children[0].addChild("K")
     print(graph.breadthFirstSearch([]))
-
+    print(graph.depth_first_search([]))
