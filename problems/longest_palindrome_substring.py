4c8fcdde26c4e12a6fe13a515d8badb7da67e1c8
Andres Ivan Montero Cassab <andres.montero.cassab@gmail.com>
2022-03-16 14:14:09 +0100

    added better solution

--- problems/longest_palindrome_substring.py
+++ problems/longest_palindrome_substring.py
@@ -15,4 +15,29 @@ def longestPalindromicSubstring(string):
     return longest_palindrome
 
 
+def get_longest_palindrome(string, left, right):
+    while left >= 0 and right < len(string):
+        if string[left] != string[right]:
+            break
+        right += 1
+        left -= 1
+    return [left + 1, right]
+
+
+def longestPalindromicSubstring_1(string):
+    # Write your code here.
+    # Time O(n^2)
+    # Space O(1)
+    longest = [0, 1]
+
+    for i in range(1, len(string)):
+        odd = get_longest_palindrome(string, i - 1, i + 1)
+        even = get_longest_palindrome(string, i - 1, i)
+        curr_longest = max(odd, even, key=lambda x: x[1] - x[0])
+        longest = max(longest, curr_longest, key=lambda x: x[1] - x[0])
+
+    return string[longest[0]: longest[1]]
+
+
 print(longestPalindromicSubstring("abaxyzzyxf"))
+print(longestPalindromicSubstring_1("abaxyzzyxf"))
