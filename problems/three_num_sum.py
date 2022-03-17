c3e12af39abdb0ab428510c3e0a9f504b74be197
Andres Ivan Montero Cassab <andres.montero.cassab@gmail.com>
2022-03-17 11:31:04 +0100

    added time and space complexity

--- problems/three_num_sum.py
+++ problems/three_num_sum.py
@@ -1,29 +1,30 @@
 def threeNumberSum(array, targetSum):
     # Write your code here.
+    # Time O(nlog(n)) + O(n^2) => O(n^2)
+    # Space O(n)
     result = []
+
 
     array.sort()
 
     for i in range(len(array)):
 
         num_1 = array[i]
-        left = i+1
+        left = i + 1
         right = len(array) - 1
-        while left < right and left < len(array) - 1 and right > i:
+        while left < right:
             num_2 = array[left]
             num_3 = array[right]
             if num_1 + num_2 + num_3 == targetSum:
                 result.append([num_1, num_2, num_3])
                 right -= 1
                 left += 1
-
             elif num_1 + num_2 + num_3 < targetSum:
                 left += 1
             else:
                 right -= 1
 
     return result
-
 
 if __name__ == '__main__':
     nums = [12, 3, 1, 2, -6, 5, -8, 6]
