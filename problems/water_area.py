3ff855395df71982159ad8873e3ef8a665d2d9df
Andres Ivan Montero Cassab <andres.montero.cassab@gmail.com>
2022-03-24 21:05:45 +0100

    water area problem improved O(1) space

--- problems/water_area.py
+++ problems/water_area.py
@@ -24,4 +24,32 @@ def waterArea(heights):
     return sum(water_areas)
 
 
+def waterArea_1(heights):
+    # Write your code here.
+    # Time O(n)
+    # Space O(1)
+    if not heights: return 0
+
+    left = 0
+    right = len(heights) - 1
+    max_left = heights[left]
+    max_right = heights[right]
+
+    total_water = 0
+    while left < right:
+        if max_left < max_right:
+            left += 1
+            total_water += max(0, max_left - heights[left])
+            max_left = max(max_left, heights[left])
+
+        else:
+            right -= 1
+            total_water += max(0, max_right - heights[right])
+            max_right = max(max_right, heights[right])
+
+    print(total_water)
+    return total_water
+
+
 print(waterArea([0, 8, 0, 0, 5, 0, 0, 10, 0, 0, 1, 1, 0, 3]))
+print(waterArea_1([0, 8, 0, 0, 5, 0, 0, 10, 0, 0, 1, 1, 0, 3]))
